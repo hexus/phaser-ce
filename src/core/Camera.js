hexus/phaser-ce

commit 03501bf71ac9855b812d68c36b35b2729c73d75c

Fixed method name typo. :full_moon_with_face:

src/physics/arcade/Body.js
@@ -1450,7 +1450,7 @@ Phaser.Physics.Arcade.Body.render = function (context, body, color, filled) {
     context.fillStyle = color;
     context.strokeStyle = color;
 
-    context.save();
+    //context.save();
 
     context.setTransform(
         cameraTransform.a,
@@ -1487,7 +1487,7 @@ Phaser.Physics.Arcade.Body.render = function (context, body, color, filled) {
         }
     }
 
-    context.restore();
+    //context.restore();
 
 };
 

src/tilemap/TilemapLayer.js
@@ -311,7 +311,7 @@ Phaser.TilemapLayer.prototype.preUpdate = function() {
 */
 Phaser.TilemapLayer.prototype.postUpdate = function () {
 
-    var cameraTransform = this.updateCameraTransform();
+    var cameraTransform = this._updateCameraTransform();
 
     if (this.fixedToCamera)
     {
@@ -320,8 +320,8 @@ Phaser.TilemapLayer.prototype.postUpdate = function () {
         // the screen, as well as scale the tiles appropriately
         this.scale.x = 1.0 / this.game.camera.scale.x;
         this.scale.y = 1.0 / this.game.camera.scale.y;
-        this.position.x = (-cameraTransform.tx + this.cameraOffset.x) / this.game.camera.scale.x;
-        this.position.y = (-cameraTransform.ty + this.cameraOffset.y) / this.game.camera.scale.y;
+        this.position.x = -cameraTransform.tx + this.cameraOffset.x / this.game.camera.scale.x;
+        this.position.y = -cameraTransform.ty + this.cameraOffset.y / this.game.camera.scale.y;
         this.scrollFactorX = 1.0 / this.game.camera.scale.x;
         this.scrollFactorY = 1.0 / this.game.camera.scale.y;
         this.tileScale.x = this.game.camera.scale.x;

commit a6cbbc2510434ae5f13becf0d9ce9ea352401959

Introduced axis-aligned camera transform.
This cleans up TilemapLayer and will be useful for bounds calculations.

Fixed a bug introduced in a previous commit (03501bf7).

src/core/Camera.js
@@ -87,9 +87,9 @@ Phaser.Camera = function (game, id, x, y, width, height) {
     this.displayObject = null;
 
     /**
-    * @property {Phaser.Point} zoom - The zoom of the camera. Control the center point of the zoom with Camera.anchor.
+    * @property {Phaser.Point} zoom - The scale of the camera. Control the center point of the scaling with Camera.anchor.
     */
-    this.zoom = new Phaser.Point(1, 1);
+    this.scale = new Phaser.Point(1, 1);
 
     /**
      * @property {number} rotation - The rotation of the camera in radians. Control the center point of the rotation with Camera.anchor.
@@ -97,7 +97,7 @@ Phaser.Camera = function (game, id, x, y, width, height) {
     this.rotation = 0;
 
     /**
-     * @property {Phaser.Point} anchor - The anchor for camera zoom and rotation. [0.5, 0.5] (center) by default.
+     * @property {Phaser.Point} anchor - The anchor for camera zoom and rotation. Set to 0.5,0.5 (center of the view) by default.
      */
     this.anchor = new Phaser.Point(0.5, 0.5);
 
@@ -152,6 +152,13 @@ Phaser.Camera = function (game, id, x, y, width, height) {
     this.transform = new Phaser.Matrix();
 
     /**
+     * The 2D transform matrix without any rotation applied.
+     * @property {Phaser.Matrix} _axisAlignedTransform
+     * @private
+     */
+    this._axisAlignedTransform = new Phaser.Matrix();
+
+    /**
     * @property {Phaser.Point} _targetPosition - Internal point used to calculate target position.
     * @private
     */
@@ -165,7 +172,7 @@ Phaser.Camera = function (game, id, x, y, width, height) {
     this._edge = 0;
 
     /**
-    * @property {Phaser.Point} _position - Current position of the camera in world.
+    * @property {Phaser.Point} _position - Current position of the camera in the game world.
     * @private
     * @default
     */
@@ -672,22 +679,21 @@ Phaser.Camera.prototype = {
         this.transform.identity();
 
         // Scale the identity matrix around the anchor
-        this.transform.tx = -anchorX;
-        this.transform.ty = -anchorY;
+        this.transform.translate(-anchorX, -anchorY);
         this.transform.scale(this.scale.x, this.scale.y);
-        this.transform.tx += anchorX;
-        this.transform.ty += anchorY;
+        this.transform.translate(anchorX, anchorY);
 
         // Apply the camera's scroll position
         this.transform.tx -= (this.view.x - this._shake.x) * this.scale.x;
         this.transform.ty -= (this.view.y - this._shake.y) * this.scale.y;
 
+        // Keep a copy of the axis-aligned transform
+        this._axisAlignedTransform.copyFrom(this.transform);
+
         // Rotate the translated matrix around the anchor
-        this.transform.tx -= anchorX;
-        this.transform.ty -= anchorY;
+        this.transform.translate(-anchorX, -anchorY);
         this.transform.rotate(this.rotation);
-        this.transform.tx += anchorX;
-        this.transform.ty += anchorY;
+        this.transform.translate(anchorX, anchorY);
 
     },
 
@@ -706,7 +712,7 @@ Phaser.Camera.prototype = {
     },
 
     /**
-    * Method called to ensure the camera doesn't venture outside of the game world.
+    * Method called to ensure the camera doesn't venture outside of its bounds.
     * Called automatically by Camera.update.
     *
     * @method Phaser.Camera#checkBounds
@@ -928,22 +934,22 @@ Object.defineProperty(Phaser.Camera.prototype, "position", {
 });
 
 /**
-* The Camera's zoom. Backwards compatibility for older versions of Phaser.
+* The Camera's zoom. An alias for camera scale.
 * @name Phaser.Camera#scale
 * @property {Phaser.Point} scale - Gets or sets the cameras scale position using Phaser.Point object.
 */
-Object.defineProperty(Phaser.Camera.prototype, "scale", {
+Object.defineProperty(Phaser.Camera.prototype, "zoom", {
 
     get: function () {
 
-        return this.zoom;
+        return this.scale;
 
     },
 
     set: function (value) {
 
-        if (typeof value.x !== "undefined") { this.zoom.x = value.x; }
-        if (typeof value.y !== "undefined") { this.zoom.y = value.y; }
+        if (typeof value.x !== "undefined") { this.scale.x = value.x; }
+        if (typeof value.y !== "undefined") { this.scale.y = value.y; }
 
         if (this.bounds)
         {
@@ -1011,6 +1017,22 @@ Object.defineProperty(Phaser.Camera.prototype, "shakeIntensity", {
     set: function (value) {
 
         this._shake.intensity = value;
+
+    }
+
+});
+
+/**
+ * The Camera's transform without rotation applied. Used for bounds calculations.
+ * @name Phaser.Camera#axisAlignedTransform
+ * @property {Phaser.Matrix}
+ * @readonly
+ */
+Object.defineProperty(Phaser.Camera.prototype, "axisAlignedTransform", {
+
+    get: function () {
+
+        return this._axisAlignedTransform;
 
     }
 

src/physics/arcade/Body.js
@@ -1450,7 +1450,7 @@ Phaser.Physics.Arcade.Body.render = function (context, body, color, filled) {
     context.fillStyle = color;
     context.strokeStyle = color;
 
-    //context.save();
+    context.save();
 
     context.setTransform(
         cameraTransform.a,
@@ -1487,7 +1487,7 @@ Phaser.Physics.Arcade.Body.render = function (context, body, color, filled) {
         }
     }
 
-    //context.restore();
+    context.restore();
 
 };
 

src/tilemap/TilemapLayer.js
@@ -311,7 +311,7 @@ Phaser.TilemapLayer.prototype.preUpdate = function() {
 */
 Phaser.TilemapLayer.prototype.postUpdate = function () {
 
-    var cameraTransform = this._updateCameraTransform();
+    var cameraTransform = this.game.camera.axisAlignedTransform;
 
     if (this.fixedToCamera)
     {
@@ -320,8 +320,8 @@ Phaser.TilemapLayer.prototype.postUpdate = function () {
         // the screen, as well as scale the tiles appropriately
         this.scale.x = 1.0 / this.game.camera.scale.x;
         this.scale.y = 1.0 / this.game.camera.scale.y;
-        this.position.x = -cameraTransform.tx + this.cameraOffset.x / this.game.camera.scale.x;
-        this.position.y = -cameraTransform.ty + this.cameraOffset.y / this.game.camera.scale.y;
+        this.position.x = (-cameraTransform.tx + this.cameraOffset.x) / this.game.camera.scale.x;
+        this.position.y = (-cameraTransform.ty + this.cameraOffset.y) / this.game.camera.scale.y;
         this.scrollFactorX = 1.0 / this.game.camera.scale.x;
         this.scrollFactorY = 1.0 / this.game.camera.scale.y;
         this.tileScale.x = this.game.camera.scale.x;
@@ -330,36 +330,6 @@ Phaser.TilemapLayer.prototype.postUpdate = function () {
 
     this._scrollX = (-cameraTransform.tx - this.tileOffset.x) * this.scrollFactorX / this.scale.x;
     this._scrollY = (-cameraTransform.ty - this.tileOffset.y) * this.scrollFactorY / this.scale.y;
-
-};
-
-/**
- * Updates the cached camera transform with its rotation undone.
- *
- * This ensures that the layer positioned correctly when the camera is rotated.
- *
- * @method Phaser.TilemapLayer#_updateCameraTransform
- * @private
- * @return {Phaser.Matrix} The updated camera transform
- */
-Phaser.TilemapLayer.prototype._updateCameraTransform = function () {
-
-    var cameraTransform = this._mc.cameraTransform;
-
-    // Update the cached copy of the camera transform
-    cameraTransform.copyFrom(this.game.camera.transform);
-
-    // Undo the rotation
-    var anchorX = this.game.camera.view.width * this.game.camera.anchor.x;
-    var anchorY = this.game.camera.view.height * this.game.camera.anchor.y;
-
-    cameraTransform.tx -= anchorX;
-    cameraTransform.ty -= anchorY;
-    cameraTransform.rotate(-this.game.camera.rotation);
-    cameraTransform.tx += anchorX;
-    cameraTransform.ty += anchorY;
-
-    return this._mc.cameraTransform;
 
 };
 